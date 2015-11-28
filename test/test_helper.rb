6e85423884fa24435fd60cca080b771b5e7e6eb9
Daniel Paul Searles <daniel.paul.searles@gmail.com>
2015-11-28 14:38:11 -0800

    Show 5 slowest tests.
    Why:
    
    * Seeing the 5 slowest tests is useful to keep the test suite fast.
    
    This change addresses the need by:
    
    * Update test configuration.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,7 @@ require 'hubgrub'
 require 'minitest/autorun'
 require 'minitest/reporters'
 
-reporter_options = { color: true }
+reporter_options = { color: true, slow_count: 5 }
 Minitest::Reporters.use!(
   [Minitest::Reporters::DefaultReporter.new(reporter_options)]
 )
